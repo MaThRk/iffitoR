d11bb143adba577c7b3d9664d251078bb222e31a
Robin Kohrs <robin.kohrs@gmx.de>
2021-01-22 09:58:11 +0100

    add example data

---
+++ R/example_data.R
@@ -0,0 +1,18 @@
+#' Example landslide data from the iffi database
+#'
+#' ...
+#'
+#'
+#' @docType data
+#'
+#' @usage data(lndsld)
+#'
+#' @format An object of class \code{"sf"}
+#'
+#' @keywords dataset
+#'
+#' @examples
+#' data(lndsld)
+#' dim(lndsld)
+
+"minimal"
