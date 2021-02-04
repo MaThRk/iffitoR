4f074edf1b48d840f687c687452d09a744c36874
Robin Kohrs <robin.kohrs@gmx.de>
2021-02-04 18:05:29 +0100

    new translations

--- R/translate.R
+++ R/translate.R
@@ -16,18 +16,18 @@ translate_iffi = function(df) {
     # rename the values in nome_movimento
     mutate(
       nome_movimento = str_replace(nome_movimento, pattern = "[Cc]omplesso", replacement = "complex"),
-      nome_movimento = str_replace(nome_movimento, pattern = "Aree soggette a crolli/ribaltamenti diffusi", replacement = "area of rockfall/topple"),
-      nome_movimento = str_replace(nome_movimento, pattern = "crollo", replacement = "rockfall"),
-      nome_movimento = str_replace(nome_movimento, pattern = "scivolamento traslativo", replacement = "translational"),
-      nome_movimento = str_replace(nome_movimento, pattern = "scivolamento rotazionale", replacement = "rotational"),
-      nome_movimento = str_replace(nome_movimento, pattern = "Aree soggette a sprofondamenti diffusi", replacement = "areas prone to sinking"),
-      nome_movimento = str_replace(nome_movimento, pattern = 'col.*lento"', replacement = "slow flow"),
-      nome_movimento = str_replace_all(nome_movimento, pattern = 'col.*rapido"', replacement = "fast flow"),
-      nome_movimento = str_replace(nome_movimento, pattern = "ribaltamento", replacement = "overturn"),
-      nome_movimento = str_replace(nome_movimento, pattern = "DGPV", replacement = "deep seated landslide"),
+      nome_movimento = str_replace(nome_movimento, pattern = "Aree soggette a crolli/ribaltamenti diffusi", replacement = "area subject to rockfall/topple"),
+      nome_movimento = str_replace(nome_movimento, pattern = "crollo", replacement = "fall-type"),
+      nome_movimento = str_replace(nome_movimento, pattern = "scivolamento traslativo", replacement = "translational slide"),
+      nome_movimento = str_replace(nome_movimento, pattern = "scivolamento rotazionale", replacement = "rotational slide"),
+      nome_movimento = str_replace(nome_movimento, pattern = "Aree soggette a sprofondamenti diffusi", replacement = "areas subject to subsidence"),
+      nome_movimento = str_replace(nome_movimento, pattern = 'col.*lento"', replacement = "slow flow-type"),
+      nome_movimento = str_replace_all(nome_movimento, pattern = 'col.*rapido"', replacement = "fast flow-type"),
+      nome_movimento = str_replace(nome_movimento, pattern = "ribaltamento", replacement = "topple"),
+      nome_movimento = str_replace(nome_movimento, pattern = "DGPV", replacement = "deep-seated movement"),
       nome_movimento = str_replace(nome_movimento, pattern = "sprofondamento", replacement = "subsidence"),
-      nome_movimento = str_replace(nome_movimento, pattern = "Aree soggette a frane superficiali diffuse", replacement = "area of diffuse surface slides"),
-      nome_movimento = str_replace(nome_movimento, pattern = "espansione", replacement = "expansion"),
+      nome_movimento = str_replace(nome_movimento, pattern = "Aree soggette a frane superficiali diffuse", replacement = "area subject to shallow-slides"),
+      nome_movimento = str_replace(nome_movimento, pattern = "espansione", replacement = "area of expansion"),
 
     ) %>%
 
@@ -35,17 +35,16 @@ translate_iffi = function(df) {
     mutate(
 
       tipologia = str_replace(tipologia, pattern="Complesso", replacement="complex"),
-      tipologia = str_replace(tipologia, pattern="Aree soggette a crolli/ribaltamenti diffusi", replacement="area of rockfall/topple"),
-      tipologia = str_replace(tipologia, pattern="Crollo/Ribaltamento", replacement="rockfall"),
+      tipologia = str_replace(tipologia, pattern="Aree soggette a crolli/ribaltamenti diffusi", replacement="area subject to rockfall/topple"),
+      tipologia = str_replace(tipologia, pattern="Crollo/Ribaltamento", replacement="fall-type/topple"),
       tipologia = str_replace(tipologia, pattern="Scivolamento rotazionale/traslativo", replacement="translational/rotational slide"),
-      tipologia = str_replace(tipologia, pattern = "Espansione", replacement = "expansion"),
-      tipologia = str_replace(tipologia, pattern = "Aree soggette a frane superficiali diffuse", replacement = "area of diffuse surface slides"),
-      tipologia = str_replace(tipologia, pattern = "Aree soggette a frane superficiali diffuse", replacement = "area of diffuse surface slides"),
-      tipologia = str_replace(tipologia, pattern = "Colamento lento", replacement = "flow (slow)"),
-      tipologia = str_replace(tipologia, pattern = "Colamento rapido", replacement = "flow (fast)"),
-      tipologia = str_replace(tipologia, pattern = "DGPV", replacement = "deep seated landslide"),
+      tipologia = str_replace(tipologia, pattern = "Espansione", replacement = "area of expansion"),
+      tipologia = str_replace(tipologia, pattern = "Aree soggette a frane superficiali diffuse", replacement = "area subject to shallow slides"),
+      tipologia = str_replace(tipologia, pattern = "Colamento lento", replacement = "slow flow-type"),
+      tipologia = str_replace(tipologia, pattern = "Colamento rapido", replacement = "fast flow-type"),
+      tipologia = str_replace(tipologia, pattern = "DGPV", replacement = "deep-seated landslide"),
       tipologia = str_replace(tipologia, pattern = "Sprofondamento", replacement = "subsidence"),
-      tipologia = str_replace(tipologia, pattern = "Aree soggette a sprofondamenti diffusi", replacement = "area of diffuse subsidence")
+      tipologia = str_replace(tipologia, pattern = "Aree soggette a sprofondamenti diffusi", replacement = "area subject to subsidence")
     ) %>%
 
     # rename the actual variables
