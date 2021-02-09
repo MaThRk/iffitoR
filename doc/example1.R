a91151065bca544b95f0dd2ee7ddd1f100639c9f
Robin Kohrs <robin.kohrs@gmx.de>
2021-02-09 09:43:34 +0100

    new vignettes

--- doc/example1.R
+++ doc/example1.R
@@ -6,6 +6,7 @@ knitr::opts_chunk$set(
 
 ## ----setup, results='hide', message=FALSE, warning=FALSE----------------------
 library(iffitoR)
+library(plotly)
 library(glue)
 library(RODBC)
 library(forcats)
@@ -51,44 +52,64 @@ attri = c("anno_min",
           "giorno_min")
 
 ## ----getdata, warning=F, message=F, cache=TRUE--------------------------------
-iffi_sf = iffitoR::make_shapefile(database_dir=database_dir,
-                                  attribute_database_name = "tbl_frane",
-                                  dictionary_database_name = "diz_frane",
-                                  shapefile = landslide_point_path,
-                                  attri = attri,
-                                  joins=joins)
+# this only works under windows
+os = Sys.info()["sysname"]
+
+if (os == "Windows") {
+  iffi_sf = iffitoR::make_shapefile(
+    database_dir = database_dir,
+    attribute_database_name = "tbl_frane",
+    dictionary_database_name = "diz_frane",
+    shapefile = landslide_point_path,
+    attri = attri,
+    joins = joins
+  )
+  
+}else{
+  iffi_sf = landsld
+}
 
 ## -----------------------------------------------------------------------------
 dplyr::glimpse(iffi_sf)
 
 ## ----translate----------------------------------------------------------------
-
-iffi_eng = iffitoR::translate_iffi(iffi_sf)
-iffi_eng %>% select(c(first_level, second_level)) %>% head()
+# if we are not working with the data from the package we still need to preprocess it a little bit
+if(os == "Windows"){
+  iffi_eng = iffitoR::translate_iffi(iffi_sf)
+  iffi_eng %>% select(c(first_level, second_level)) %>% head()
+}
 
 ## ----getdate------------------------------------------------------------------
-iffi_date = iffitoR::get_date_information(iffi_eng)
-iffi_date %>% select(matches("date|year|month|day")) %>% glimpse()
+# same for the time information that is alrady in the data from the package
+if(os=="Windows"){
+  iffi_date = iffitoR::get_date_information(iffi_eng)
+  iffi_date %>% select(matches("date|year|month|day")) %>% glimpse()
+}
 
 ## -----------------------------------------------------------------------------
+if(os=="Windows"){
 iffi_sf_poly = iffitoR::make_shapefile(database_dir=database_dir,
                                   attribute_database_name = "tbl_frane",
                                   dictionary_database_name = "diz_frane",
                                   shapefile = landslide_poly_path,
                                   attri = attri,
                                   joins=joins)
+}
 
-## ----polydimension------------------------------------------------------------
+# how many polygons do we have?
 dim(iffi_sf_poly)
 
 ## ----translatepoly------------------------------------------------------------
+if(os=="Windows"){
 #trasnlate
 iffi_sf_poly = iffitoR::translate_iffi(iffi_sf_poly)
 
 # get date information
 iffi_sf_poly = iffitoR::get_date_information(iffi_sf_poly)
+}
 
 ## ---- fig.width=8-------------------------------------------------------------
+if(os=="Windows"){
 iffi_sf_poly %>% 
   count(second_level, sort=T) %>% 
   mutate(second_level = glue("{second_level} ({n})")) %>% 
@@ -96,12 +117,14 @@ iffi_sf_poly %>%
   ggplot(aes(x=n, y=second_level)) +
   geom_col()  +
   labs(title="Distribution of polygons in the iffi database",
+       subtitle = "in the second-level classification",
        x = "# of events",
        y = "") +
   theme_light()
+}
 
 ## ----whenslides, fig.width=10-------------------------------------------------
-
+if(os=="Windows"){
 iffi_sf_poly %>% 
   filter(year.int >= 1990) %>% 
   filter(str_detect(second_level, "transla|rota")) %>% 
@@ -114,5 +137,30 @@ iffi_sf_poly %>%
   theme(
     axis.text.x = element_text(angle=90)
   )
+}
 
+## ----plotpoints, fig.width=8--------------------------------------------------
+landsld %>% 
+  # filter(year.int > 1980) %>% 
+  count(second_level, sort = TRUE) %>% 
+  mutate(second_level = glue("{second_level} ({n})")) %>% 
+  mutate(second_level = fct_reorder(second_level, n)) %>% 
+  ggplot() +
+  geom_col(aes(x = n, y=second_level)) +
+  labs(title="Distribution of points in the iffi database",
+       subtitle = "in the second-level classification",
+       x = "# of events",
+       y = "") +
+  theme_light()
+
+## ----timepoints, fig.width=8--------------------------------------------------
+landsld %>% 
+  filter(str_detect(second_level, "rotational|translational")) %>% 
+  filter(date_info != "no date") %>% 
+  group_by(year.int) %>% 
+  mutate(n = sum(year.int)) %>% 
+  plot_ly(., x = ~year.int,  type="histogram", color=~second_level) %>% 
+  layout(title="Distriubtion of rotational and translational slides", 
+         xaxis = list("year", zeroline=T), 
+         yaxis = list("# of events"))
 
