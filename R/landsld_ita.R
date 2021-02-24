0c03b98f43e2dfd05b88da00448b3da66bcc7530
Robin Kohrs <robin.kohrs@gmx.de>
2021-02-24 13:39:56 +0100

    add italian data

---
+++ R/landsld_ita.R
@@ -0,0 +1,12 @@
+#' Example landslide data from the iffi database with italian column-names
+#'
+#' @docType data
+#'
+#' @usage data(landsld_ita)
+#'
+#' @format An object of class \code{"sf"}
+#'
+#' @keywords dataset
+#'
+
+"landsld_ita"
